62890eb94dc685d67911d1fa7a635276a3a867e7
Alec Siikaluoma <alec.siikaluoma@helsinki.fi>
2018-11-25 10:08:10 +0200

    viikko 4 tehtäviä

--- koodi/viikko4/MatkakorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
+++ koodi/viikko4/MatkakorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
@@ -40,5 +40,19 @@ public class KassapaateTest {
         verify(kortti, times(1)).getSaldo();
         verify(kortti, times(0)).osta(anyInt());
     }
+    
+    @Test
+    public void lataaLisaaRahamaaranKortille() {
+        kassa.lataa(kortti, 5);
+        
+        verify(kortti, times(1)).lataa(anyInt());
+    }
+    
+        @Test
+    public void lataaLisaaVaaraRahamaaraKortille() {
+        kassa.lataa(kortti, -5);
+        
+        verify(kortti, times(0)).lataa(anyInt());
+    }
       
 }
