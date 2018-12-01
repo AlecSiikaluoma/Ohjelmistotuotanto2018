90108eceb1532ea650bd07b66ce3740d0d49cee6
Alec Siikaluoma <alec.siikaluoma@helsinki.fi>
2018-12-01 10:24:55 +0200

    Tehtävä 2

--- koodi/viikko5/Tennis/src/main/java/ohtu/TennisGame.java
+++ koodi/viikko5/Tennis/src/main/java/ohtu/TennisGame.java
@@ -19,42 +19,45 @@ public class TennisGame {
             m_score2 += 1;
     }
 
-    public String getScore() {
+    
+    private String printTied() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
+        switch (m_score1)
             {
                 case 0:
-                        score = "Love-All";
+                    score = "Love-All";
                     break;
                 case 1:
-                        score = "Fifteen-All";
+                    score = "Fifteen-All";
                     break;
                 case 2:
-                        score = "Thirty-All";
+                    score = "Thirty-All";
                     break;
                 case 3:
-                        score = "Forty-All";
+                    score = "Forty-All";
                     break;
                 default:
-                        score = "Deuce";
+                    score = "Deuce";
                     break;
                 
             }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
+        return score;
+    }
+    
+    private String printAdvantage() {
+        String score = "";
+        int minusResult = m_score1-m_score2;
+        if (minusResult==1) score ="Advantage player1";
+        else if (minusResult ==-1) score ="Advantage player2";
+        else if (minusResult>=2) score = "Win for player1";
+        else score ="Win for player2";
+        return score;
+    }
+    
+    private String printSituation() {
+        int tempScore=0;
+        String score = "";
+        for (int i=1; i<3; i++)
             {
                 if (i==1) tempScore = m_score1;
                 else { score+="-"; tempScore = m_score2;}
@@ -74,7 +77,21 @@ public class TennisGame {
                         break;
                 }
             }
-        }
         return score;
+    }
+    
+    public String getScore() {
+        if (m_score1==m_score2)
+        {
+           return printTied();
+        }
+        else if (m_score1>=4 || m_score2>=4)
+        {
+            return printAdvantage();
+        }
+        else
+        {
+            return printSituation();
+        }
     }
 }
